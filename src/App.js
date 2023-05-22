e1e152907c8d771badb5aace7800c62d166152f6
Adson Tanajura <adson.tanajura@gmail.com>
2023-05-22 09:26:42 -0300

    Start Project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from './Components/Header';
+import Amor from './Components/Amor';
+
+function App() {
+    return (
+    <>
+          <Header />
+          <Header />
+          <Amor/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/Amor.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Amor() {
+    return <h1>EU TE AMO MUITO MARI!! S2</h1>
+}
+
+
